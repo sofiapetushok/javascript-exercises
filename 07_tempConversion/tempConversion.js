c76dd9c9951b13c26e0f5952d034c91dc51ce84a
sofiapetushok <sofiapetushok@outlook.com>
2023-09-30 20:37:48 +0300

    exercise solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  cDeg = (temp - 32) / 1.8000;
+  rounded = Math.round(cDeg * 10) / 10;
+  if (temp < 0) {
+    rounded = Math.round(cDeg * 10) / 10;
+  }
+  if (cDeg === 0) {
+    rounded = 0;
+  }   
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fDeg = temp*1.8000 + 32.00;
+  if (temp < 0) {
+    rounded = Math.round(fDeg * 10) / 10;
+  } else if (fDeg === 0) {
+    rounded = 0;
+  } else {
+    rounded = Math.round(fDeg * 10) / 10;
+  }
+  return rounded;
 };
 
 // Do not edit below this line
