074ddcc2339b0d6805bb2f140bde217ed553e64c
sofiapetushok <sofiapetushok@outlook.com>
2023-10-14 17:47:59 +0300

    Palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,20 @@
 const palindromes = function (string) {
-    const characters = string.split('');
+    const characters = string.split('').reverse().join('');
 
-    const stringNoSymbols = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    const stringNoSymbols = characters.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
     const loweredString = stringNoSymbols.toLowerCase();
-    const reversedString = loweredString.
+    const reversedString = loweredString.split('').reverse().join('');
+    
+    console.log("Processed:"+loweredString);
+    
+    console.log("Reversed :"+reversedString);
+    if (loweredString == reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
-palindromes("djfijeos,ijfs! AWea ! esdlskme , . and . and + ");
+// palindromes("djfijeos,ijfs! AWea ! esdlskme , . and . and + ");
 // Do not edit below this line
-// module.exports = palindromes;
+module.exports = palindromes;
